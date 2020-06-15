c27ca29419ced6260e29a0d7938591916635c6b5
Matthew Nelson <developer@matthewnelson.io>
2020-06-14 22:47:56 -0400

    build out the broadcast receiver and stub out the service

--- topl-android-service/src/main/java/io/matthewnelson/topl_android_service/TorService.kt
+++ topl-android-service/src/main/java/io/matthewnelson/topl_android_service/TorService.kt
@@ -6,8 +6,37 @@ import android.os.IBinder
 
 internal class TorService: Service() {
 
-    override fun onBind(intent: Intent?): IBinder? {
-        TODO("Not yet implemented")
+    companion object {
+
     }
 
+    override fun onBind(intent: Intent?): IBinder? {
+        // TODO: Implement
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        // TODO: Implement
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // TODO: Implement
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        // TODO: Implement
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+        // TODO: Implement
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        super.onTaskRemoved(rootIntent)
+        // TODO: Implement
+    }
 }
--- topl-android-service/src/main/java/io/matthewnelson/topl_android_service/TorServiceReceiver.kt
+++ topl-android-service/src/main/java/io/matthewnelson/topl_android_service/TorServiceReceiver.kt
@@ -1,4 +1,41 @@
 package io.matthewnelson.topl_android_service
 
-internal class TorServiceReceiver {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import java.math.BigInteger
+import java.security.SecureRandom
+
+internal class TorServiceReceiver: BroadcastReceiver() {
+
+    companion object {
+        val TOR_SERVICE_START: String = BigInteger(130, SecureRandom()).toString(32)
+        val TOR_SERVICE_STOP: String = BigInteger(130, SecureRandom()).toString(32)
+        val TOR_SERVICE_RESTART: String = BigInteger(130, SecureRandom()).toString(32)
+        val TOR_SERVICE_RENEW: String = BigInteger(130, SecureRandom()).toString(32)
+
+        const val EXTRAS = "EXTRAS"
+        const val UPDATE_SETTINGS = "UPDATE_SETTINGS"
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (context != null && intent != null) {
+            when (val action = intent.action) {
+                TOR_SERVICE_START, TOR_SERVICE_STOP, TOR_SERVICE_RESTART, TOR_SERVICE_RENEW -> {
+                    val torServiceIntent = Intent(context.applicationContext, TorService::class.java)
+                    torServiceIntent.putExtra(EXTRAS, intent.getBooleanExtra(UPDATE_SETTINGS, false))
+                    torServiceIntent.action = action
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        context.startForegroundService(torServiceIntent)
+                    } else {
+                        context.startService(torServiceIntent)
+                    }
+                }
+                else -> { }
+            }
+        }
+    }
+
 }
