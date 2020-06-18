234aeb781fc6762bbe7de202387d71c087c14909
Matthew Nelson <developer@matthewnelson.io>
2020-06-18 14:17:59 -0400

    add buttons to the sample app and move starting of Tor to the main activity

--- sampleapp/src/main/java/io/matthewnelson/sampleapp/App.kt
+++ sampleapp/src/main/java/io/matthewnelson/sampleapp/App.kt
@@ -1,6 +1,7 @@
 package io.matthewnelson.sampleapp
 
 import android.app.Application
+import android.content.Context
 import io.matthewnelson.topl_android_service.TorServiceController
 
 class App: Application() {
@@ -8,17 +9,24 @@ class App: Application() {
     override fun onCreate() {
         super.onCreate()
 
+        setupTorServices(this)
+    }
+
+    private fun setupTorServices(context: Context) {
         TorServiceController.Builder(
-            this,
+            context.applicationContext,
             BuildConfig.VERSION_CODE,
             MyTorSettings(),
             "common/geoip",
             "common/geoip6"
         )
-            .customizeNotification("My Sample Application", 6156, MainActivity::class.java)
+            .customizeNotification(
+                "TorService Channel",
+                "Tor Channel",
+                "My Sample Application",
+                6156)
+            .setActivityToBeOpenedOnTap(MainActivity::class.java, null, null)
             .applyNotificationSettings()
             .build()
-
-        TorServiceController.startTor()
     }
 }
--- sampleapp/src/main/java/io/matthewnelson/sampleapp/MainActivity.kt
+++ sampleapp/src/main/java/io/matthewnelson/sampleapp/MainActivity.kt
@@ -2,11 +2,32 @@ package io.matthewnelson.sampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import io.matthewnelson.topl_android_service.TorServiceController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonStart = findViewById<Button>(R.id.button_start)
+        val buttonStop = findViewById<Button>(R.id.button_stop)
+        val buttonRestart = findViewById<Button>(R.id.button_restart)
+        val buttonNewId = findViewById<Button>(R.id.button_new_identity)
+        TorServiceController.startTor()
+
+        buttonStart.setOnClickListener {
+            TorServiceController.startTor()
+        }
+        buttonStop.setOnClickListener {
+            TorServiceController.stopTor()
+        }
+        buttonRestart.setOnClickListener {
+            TorServiceController.restartTor()
+        }
+        buttonNewId.setOnClickListener {
+            TorServiceController.newIdentity()
+        }
     }
 }
