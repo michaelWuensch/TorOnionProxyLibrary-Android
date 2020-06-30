michaelWuensch/TorOnionProxyLibrary-Android

commit 63b44bc567c18b7bca2926638691a9771ce86dfc

clean up check for being bootstrapped, and implement the check into networkstate changes

topl-service/src/main/java/io/matthewnelson/topl_service/onionproxy/OnionProxyEventBroadcaster.kt
@@ -63,7 +63,7 @@ internal class OnionProxyEventBroadcaster(
         // Only update the notification if proper State is had & we're bootstrapped.
         if (torState == TorState.ON &&
             torNetworkState == TorNetworkState.ENABLED &&
-            bootstrapProgress == "Bootstrapped 100%"
+            isBootstrappingComplete()
         ) {
             if (read != this.bytesRead || written != this.bytesWritten) {
                 this.bytesRead = read
@@ -145,6 +145,8 @@ internal class OnionProxyEventBroadcaster(
     ///////////////
     private lateinit var noticeMsgToContentTextJob: Job
     private var bootstrapProgress = ""
+    private fun isBootstrappingComplete(): Boolean =
+        bootstrapProgress == "Bootstrapped 100%"
 
     override fun broadcastNotice(msg: String) {
 
@@ -156,7 +158,7 @@ internal class OnionProxyEventBroadcaster(
             if (bootstrapped != bootstrapProgress) {
                 serviceNotification.updateContentText(bootstrapped)
 
-                if (bootstrapped == "Bootstrapped 100%") {
+                if (isBootstrappingComplete()) {
                     serviceNotification.updateIcon(torService, ImageState.ENABLED)
                     serviceNotification.addActions(torService)
                 }
@@ -204,7 +206,7 @@ internal class OnionProxyEventBroadcaster(
                     getFormattedBandwidthString(bytesRead, bytesWritten)
                 )
             } else {
-                if (bootstrapProgress == "Bootstrapped 100%")
+                if (isBootstrappingComplete())
                     serviceNotification.updateContentText(getFormattedBandwidthString(0L, 0L))
             }
         }
@@ -235,7 +237,9 @@ internal class OnionProxyEventBroadcaster(
             torNetworkState = networkState
             serviceNotification.updateIcon(torService, ImageState.DISABLED)
         } else {
-            serviceNotification.updateIcon(torService, ImageState.ENABLED)
+            if (isBootstrappingComplete())
+                serviceNotification.updateIcon(torService, ImageState.ENABLED)
+
             // Update torNetworkState _after_ setting the icon to `enabled` so bandwidth changes
             // occur afterwards and this won't overwrite ImageState.DATA
             torNetworkState = networkState

commit 554859fccb312e1a9425b7e973b08105d978b444

fix bug introduced from 63b44bc567c18b7bca2926638691a9771ce86dfc

topl-service/src/main/java/io/matthewnelson/topl_service/onionproxy/OnionProxyEventBroadcaster.kt
@@ -164,7 +164,7 @@ internal class OnionProxyEventBroadcaster(
             if (bootstrapped != bootstrapProgress) {
                 serviceNotification.updateContentText(bootstrapped)
 
-                if (isBootstrappingComplete()) {
+                if (bootstrapped == "Bootstrapped 100%") {
                     serviceNotification.updateIcon(torService, ImageState.ENABLED)
                     serviceNotification.addActions(torService)
                 }