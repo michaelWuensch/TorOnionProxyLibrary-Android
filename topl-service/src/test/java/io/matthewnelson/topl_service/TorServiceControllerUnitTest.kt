405bb1deb6a0c56cf05db4ae1e666e94af73f0a4
Matthew Nelson <developer@matthewnelson.io>
2020-07-28 06:36:38 -0400

    ensure tests run in a fixed order

--- topl-service/src/test/java/io/matthewnelson/topl_service/TorServiceControllerUnitTest.kt
+++ topl-service/src/test/java/io/matthewnelson/topl_service/TorServiceControllerUnitTest.kt
@@ -2,17 +2,24 @@ package io.matthewnelson.topl_service
 
 import android.app.Application
 import androidx.test.core.app.ApplicationProvider
+import io.matthewnelson.test_helpers.TestEventBroadcaster
 import io.matthewnelson.test_helpers.TestTorSettings
 import io.matthewnelson.topl_core_base.TorConfigFiles
 import io.matthewnelson.topl_service.notification.ServiceNotification
+import io.matthewnelson.topl_service.service.TorService
 import org.junit.Before
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertNotNull
+import org.junit.FixMethodOrder
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import java.io.File
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @Config(minSdk = 16, maxSdk = 28)
 @RunWith(RobolectricTestRunner::class)
 internal class TorServiceControllerUnitTest {
@@ -50,29 +57,39 @@ internal class TorServiceControllerUnitTest {
     }
 
     @Test(expected = RuntimeException::class)
-    fun `throw errors if startTor called before build`() {
+    fun `_throw errors if startTor called before build`() {
         TorServiceController.startTor()
     }
 
     @Test(expected = RuntimeException::class)
-    fun `throw errors if sendBroadcast called before build`() {
+    fun `_throw errors if sendBroadcast called before build`() {
         // sendBroadcast method used in newIdentity, restartTor, and stopTor
         // which is what will throw the RuntimeException.
         TorServiceController.newIdentity()
     }
 
     @Test(expected = RuntimeException::class)
-    fun `throw errors if getTorSettings called before build`() {
+    fun `_throw errors if getTorSettings called before build`() {
         TorServiceController.getTorSettings()
     }
 
     @Test(expected = RuntimeException::class)
-    fun `throw errors if getTorConfigFiles called before build`() {
+    fun `_throw errors if getTorConfigFiles called before build`() {
         TorServiceController.getTorConfigFiles()
     }
 
     @Test
-    fun `ensure one-time initialization`() {
+    fun `_z_ensure setBuildConfigDebug is properly initialized`() {
+        // Hasn't been initialized yet
+        assertNull(TorService.buildConfigDebug)
+
+        builder.setBuildConfigDebug(BuildConfig.DEBUG).build()
+
+        assertEquals(TorService.buildConfigDebug, BuildConfig.DEBUG)
+    }
+
+    @Test
+    fun `ensure one-time initialization if build called more than once`() {
         builder.build()
         val initialHashCode = TorServiceController.getTorSettings().hashCode()
 
