f64f2337c63ce89cb619d69aa76a69193f2c55f0
Matthew Nelson <developer@matthewnelson.io>
2020-12-11 12:36:27 -0500

    update documentation and make methods open instead of abstract

--- topl-service-base/src/main/java/io/matthewnelson/topl_service_base/ServiceExecutionHooks.kt
+++ topl-service-base/src/main/java/io/matthewnelson/topl_service_base/ServiceExecutionHooks.kt
@@ -8,17 +8,35 @@ import android.content.Context
 abstract class ServiceExecutionHooks {
 
     /**
-     * Is executed from TorService.onCreate on Dispatchers.Main
+     * Is executed from TorService.onCreate on Dispatchers.Default launched from
+     * it's own coroutine.
      * */
-    abstract suspend fun executeOnCreateTorService(context: Context)
+    open suspend fun executeOnCreateTorService(context: Context) {}
 
     /**
-     * Is executed from ServiceActionProcessor.processQueue on Dispatchers.Main
+     * Is executed from TorService.startTor on Dispatchers.IO
+     *
+     * **WARNING**: Indefinitely suspending the coroutine (ex. collecting
+     * Flow or something) will inhibit the reset of the `startTor` method from
+     * executing; perform those tasks from [executeOnCreateTorService].
+     *
+     * This is meant to provide synchronous execution prior to the
+     * start of Tor.
+     *
+     * **NOTE**: Exceptions thrown from your implementation will inhibit starting
+     * Tor and will be broadcast via the EventBroadcaster.
      * */
-    abstract suspend fun executeBeforeStartTor(context: Context)
+    open suspend fun executeBeforeStartTor(context: Context) {}
 
     /**
-     * Is executed from ServiceActionProcessor.processQueue on Dispatchers.Main
+     * Is executed from TorService.stopTor on Dispatchers.IO
+     *
+     * **WARNING**: Indefinitely suspending the coroutine (ex. collecting
+     * Flow or something) will inhibit the reset of the `stopTor` method from
+     * executing; perform those tasks from [executeOnCreateTorService].
+     *
+     * This is meant to provide synchronous execution of code after Tor has been
+     * stopped, prior to TorService being stopped.
      * */
-    abstract suspend fun executeAfterStopTor(context: Context)
+    open suspend fun executeAfterStopTor(context: Context) {}
 }
